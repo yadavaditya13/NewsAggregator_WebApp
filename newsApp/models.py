b6d8cf84accca78cadcad90f9bb5320d5fb391d2
Aditya Yadav <aditya78624@gmail.com>
2021-04-20 16:25:05 +0530

    updated database

--- newsApp/models.py
+++ newsApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Headline(models.Model):
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, primary_key=True)
     image = models.URLField(null=True, blank=True)
     url = models.TextField()
     content = models.TextField(null=True, blank=True)
